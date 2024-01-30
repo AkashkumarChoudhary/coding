29bc4af2dde5cf5f1564d4317c76206e829078ba
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-01-30 20:55:56 +0530

    30jan

---
+++ dsa/array/output/leftrotatebyk.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+void Rotatetoleft(int arr[], int n, int k)
+{
+  if (n == 0)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  int temp[k];
+  for (int i = 0; i < k; i++)
+  {
+    temp[i] = arr[i];
+  }
+  for (int i = 0; i < n - k; i++)
+  {
+    arr[i] = arr[i + k];
+  }
+  for (int i = n - k; i < n; i++)
+  {
+    arr[i] = temp[i - n + k];
+  }
+}
+int main()
+{
+  int n = 7;
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  int k = 2;
+  Rotatetoleft(arr, n, k);
+  cout << "After Rotating the elements to left " << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
