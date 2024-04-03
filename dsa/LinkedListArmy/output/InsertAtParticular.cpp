348c10dc0dd4ec1da56098c8a6e0e54443381ed7
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-04-03 20:30:20 +0530

    04/03/2024

---
+++ dsa/LinkedListArmy/output/InsertAtParticular.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data ;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value ;
+        next = NULL;
+    }
+};
+
+Node CreateLinkedList (int arr[],int index, int size)
+{
+    // Base case
+    if(index == size)
+    return NULL;
+
+    Node* temp;
+    temp = new Node(arr[index]);
+    temp->next = CreateLinkedList(arr, index+1 , size);
+
+    return temp;
+
+}
+
+int main()
+{
+    Node* Head ;
+    Head = NULL;
+
+    int arr[] ={ 2, 4, 5,6 , 7,8};
+
+    Head = CreateLinkedList(arr , 0 ,5);
+
+    // Insert Node at particular Position
+    int x = 3; //Insert POsition
+
+    int value = 30;
+
+    Node* temp = Head;
+    x-- ;
+
+    while(x--)
+    {
+        temp = temp -> next;
+
+    };
+
+    Node* temp2 = 
+
+}
