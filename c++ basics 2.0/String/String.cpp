692ee598142d403db24309f28e0f3d575d61de61
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-07-22 22:52:43 +0530

    22

---
+++ c++ basics 2.0/String/String.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // char arr[] = { 'a', 'p','p','l', 'e'};
+    // cout<< arr;
+    // char arr[10] ;
+    // cin>>arr;
+    // arr[2] = '\0';
+    // cout<<arr;
+
+    string s;
+    getline(cin,s);
+    cout<<s; 
+
+
+}
