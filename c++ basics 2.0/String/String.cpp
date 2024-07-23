17a06d1dcd8a802b5c84a44c516da649b51c3847
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-07-23 22:10:01 +0530

    23

--- c++ basics 2.0/String/String.cpp
+++ c++ basics 2.0/String/String.cpp
@@ -9,9 +9,49 @@ int main(){
     // arr[2] = '\0';
     // cout<<arr;
 
-    string s;
-    getline(cin,s);
-    cout<<s; 
+    // string s;
+    // getline(cin,s);
+    // cout<<s; 
+    // cout<<s.size();
 
+    // string s1 = "rohit" ,s2= "akash";
+    // string s3 = s1+ s2;
+    // string s3 = s1.append(s2);
+    // s1.push_back('p');
+    // s1.pop_back();
+
+    // cout<<s1;
+    // cout<<s3;
+
+    // string s = "akash is a \"good\" boy";
+    // string s = "\\0";
+    // cout<<s;
+
+    // reverse of an string
+    string s = "rohit";
+    int start = 0 ,end = s.size()-1;
+    while(start < end){
+        swap(s[start], s[end]);
+        start++ , end--;
+    }
+
+    cout<<s;
+    int size = 0;
+    while(s[size]!= '\0'){
+        size++;
+    }
+    cout<<endl;
+    cout<< size<< " ";
+
+    string s2 = "naman";
+    start = 0 ; end = s2.size()-1;
+    while(start<end){
+        if(s2[start] != s2[end]){
+            cout<< " it is not a pallindrome";
+            return 0;
+        }
+        start++, end--;
+    }
+    cout<< "it is a pallindrome";
 
 }
